fb434b35c95c271555ef411f5ba60b2eb1232979
Brian Nolan <bp.nolan08@gmail.com>
2017-01-20 00:23:55 -0500

    moved fortune cookie into module

--- firstsite.js
+++ firstsite.js
@@ -1,7 +1,8 @@
 var express = require('express');
-
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout : 'main'});
+var fortune = require('./lib/fortune.js')
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -10,22 +11,12 @@ app.set('port', process.env.port || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-
-var fortunes = [
-    'you will gain 100 mmr today',
-    'you will have a peasant surprise',
-    'myes',
-    'you will lose 100 mmr today',
-    'you will meet a sloth today'
-];
-
 app.get('/', function(req, res){
     res.render('home');
 });
 
 app.get('/about', function(req, res){
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', {fortune : randomFortune});
+    res.render('about', {fortune : fortune.getFortune()});
 });
 
 /*custom 404 page*/
